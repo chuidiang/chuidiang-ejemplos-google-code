11d83e4fedf9bc085b62d13ded1de10d141d8e5e
chuidiang <chuidiang@1f131f20-f6db-11dd-b556-9305e745af9e>
2009-02-10 18:38:27 +0000

    Comentado codigo y añadido metodo para nueva forma de asociacion de evento a persona.

--- ejemplo-hibernate/src/main/java/com/chuidiang/ejemplos/hibernate/ejemplo1/Ejemplo1.java
+++ ejemplo-hibernate/src/main/java/com/chuidiang/ejemplos/hibernate/ejemplo1/Ejemplo1.java
@@ -8,13 +8,17 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
-import com.chuidiang.ejemplos.hibernate.ejemplo1.Event;
-
+/**
+ * Ejemplo sencillo con Hibernate.<br>
+ * Se crea un Evento y se inserta en base de datos. Luego se hace la consulta y
+ * se presenta por pantalla.
+ * 
+ * @author Chuidiang
+ * 
+ */
 public class Ejemplo1 {
     private final static Logger log = Logger.getLogger(Ejemplo1.class);
-    /**
-     * @param args
-     */
+
     public static void main(String[] args) {
         BasicConfigurator.configure();
         Logger.getLogger("org.hibernate").setLevel(Level.WARN);
@@ -22,31 +26,46 @@ public class Ejemplo1 {
     }
 
     public Ejemplo1() {
+        // Se inserta un evento en bd
         createAndStoreEvent("El Event", new Date());
+
+        // Se hace la consulta y se lista
         listEvents();
         HibernateUtil.getSessionFactory().close();
     }
 
     private Long createAndStoreEvent(String title, Date theDate) {
+        // Obtener session de hibernate y comenzar transaccion
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
+
+        // Creacion del evento
         Event theEvent = new Event();
         theEvent.setTitle(title);
         theEvent.setDate(theDate);
+
+        // Insercion en base de datos.
         session.save(theEvent);
         session.getTransaction().commit();
-        log.info("Insertado: "+theEvent);
+        log.info("Insertado: " + theEvent);
         return theEvent.getId();
     }
 
     private List<Event> listEvents() {
+        // Obtener sesion de hibernate y comenzar transaccion
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
-        List<Event> result = (List<Event>)session.createQuery("from Event").list();
-        session.getTransaction().commit();
+
+        // Consulta y mostrar resultados.
+        List<Event> result = (List<Event>) session.createQuery("from Event")
+                .list();
         for (Event evento : result) {
-            log.info("Leido: "+evento);
+            log.info("Leido: " + evento);
         }
+
+        // Cierre de sesion.
+        session.getTransaction().commit();
+
         return result;
     }
 
--- ejemplo-hibernate/src/main/java/com/chuidiang/ejemplos/hibernate/ejemplo2/Ejemplo2.java
+++ ejemplo-hibernate/src/main/java/com/chuidiang/ejemplos/hibernate/ejemplo2/Ejemplo2.java
@@ -10,6 +10,15 @@ import org.hibernate.Session;
 
 import com.chuidiang.ejemplos.hibernate.ejemplo1.Event;
 
+/**
+ * Ejemplo sencillo de Hibernate con una asociación entre dos tablas.<br>
+ * La clase Person puede tener asociados varios Event. Se inserta una Person en
+ * base de datos, luego un Event, se asocian y luego se hace una consulta para
+ * mostrar los resultados
+ * 
+ * @author Chuidiang
+ * 
+ */
 public class Ejemplo2 {
 
     public static void main(String[] args) {
@@ -20,10 +29,19 @@ public class Ejemplo2 {
     }
 
     public Ejemplo2() {
+        // Se crea y guarda en bd un evento
         Long idEvent = createAndStoreEvent("El Event", new Date());
+
+        // Se crea y guarda en bd una persona
         Long idPerson = createAndStorePerson("Juan", "Cortés", 34);
+
+        // Se asocian en bd persona y evento
         addPersonToEvent(idPerson, idEvent);
 
+        // Se añade un nuevo evento a la persona
+        addNewEventToPerson("Nuevo evento", new Date(), idPerson);
+
+        // Se listan las personas con sus eventos
         listPersons();
     }
 
@@ -39,12 +57,17 @@ public class Ejemplo2 {
     }
 
     private Long createAndStorePerson(String nombre, String apellido, int edad) {
+        // Se obtiene la sesion de hibernate
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
+
+        // Creación de la persona.
         Person p = new Person();
         p.setAge(edad);
         p.setFirstname(nombre);
         p.setLastname(apellido);
+
+        // Se guardaen bd y se cierra la sesion
         session.save(p);
         session.getTransaction().commit();
         return p.getId();
@@ -54,19 +77,54 @@ public class Ejemplo2 {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
         List<Person> result = session.createQuery("from Person").list();
+
+        // Debe hacerse el listado antes del commit, puesto que el toString()
+        // de Person consulta los Event asociados a la persona.
         for (Person persona : result) {
             System.out.println(persona);
         }
+
+        // Cierre de sesion
         session.getTransaction().commit();
         return result;
     }
 
     private void addPersonToEvent(Long personId, Long eventId) {
+        // Sesion
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
+
+        // Se cargan perons y eventos
         Person aPerson = (Person) session.load(Person.class, personId);
         Event anEvent = (Event) session.load(Event.class, eventId);
+
+        // Se añade el evento a la persona
         aPerson.getEvents().add(anEvent);
+
+        // Se termina la transaccion
+        session.getTransaction().commit();
+    }
+
+    private void addNewEventToPerson(String eventName, Date eventDate,
+            Long personId) {
+        // Sesion
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+
+        // Se carga la persona de bd
+        Person aPerson = (Person) session.load(Person.class, personId);
+
+        // Se crea un evento y se salva
+        Event e = new Event();
+        e.setTitle(eventName);
+        e.setDate(eventDate);
+        session.save(e);
+
+        // Se añade el evento a la persona y se salva
+        aPerson.getEvents().add(e);
+        session.save(aPerson);
+
+        // fin de sesion
         session.getTransaction().commit();
     }
 }
