6f1e1e058212d6447143b23331a849a626345b2c
chuidiang <chuidiang@1f131f20-f6db-11dd-b556-9305e745af9e>
2009-02-10 18:16:47 +0000

    Ejemplo 2. Mapeando una asociación con Hibernate

--- ejemplo-hibernate/src/main/java/com/chuidiang/ejemplos/hibernate/ejemplo2/Person.java
+++ ejemplo-hibernate/src/main/java/com/chuidiang/ejemplos/hibernate/ejemplo2/Person.java
@@ -1,44 +1,70 @@
 package com.chuidiang.ejemplos.hibernate.ejemplo2;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import com.chuidiang.ejemplos.hibernate.ejemplo1.Event;
 
 public class Person {
     private Long id;
     private int age;
     private String firstname;
     private String lastname;
-    public Person() {}
+
+    public Person() {
+    }
+
     public Long getId() {
         return id;
     }
-////    private Set events = new HashSet();
-////    public Set getEvents() {
-//    return events;
-//    }
-//    public void setEvents(Set events) {
-//    this.events = events;
-//    }
+
+    private Set events = new HashSet();
+
+    public Set getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set events) {
+        this.events = events;
+    }
+
     private void setId(Long id) {
         this.id = id;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public String getFirstname() {
         return firstname;
     }
+
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
+
     public String getLastname() {
         return lastname;
     }
+
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-    public String toString () {
-        return id + " - " +firstname + " - " +lastname + " - " +age;
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona: " + id + " - " + firstname + " - " + lastname
+                + " - " + age);
+        Iterator<Event> eventos = getEvents().iterator();
+        while (eventos.hasNext())
+            sb.append(System.getProperty("line.separator") + "   Evento : "
+                    + eventos.next());
+        return sb.toString();
     }
 }
