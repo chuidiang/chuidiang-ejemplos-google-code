b7fd320ab760e415a8924a89a47b96eb06390148
chuidiang <chuidiang@1f131f20-f6db-11dd-b556-9305e745af9e>
2009-02-12 20:19:05 +0000

    Ejemplo con hibernate annotations

---
+++ ejemplo-hibernate-annotations/src/main/java/com/chuidiang/ejemplos/hibernate_annotations/HibernateUtil.java
@@ -0,0 +1,35 @@
+package com.chuidiang.ejemplos.hibernate_annotations;
+
+import java.io.File;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ * Clase de utilidad para obtener la sesion de hibernate.
+ * 
+ * @author Chuidiang
+ * 
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Si no ponemos fichero, intenta cargar "/hibernate.cfg.xml" en el
+            // raiz
+            sessionFactory = new AnnotationConfiguration().configure(
+                    new File("hibernate.cfg.xml")).buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log exception!
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return sessionFactory.openSession();
+    }
+}
