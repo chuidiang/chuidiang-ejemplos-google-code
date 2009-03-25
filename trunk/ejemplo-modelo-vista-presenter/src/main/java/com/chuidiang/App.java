48c2062b15f181cbf6dcbae6607d30bf44658b4b
chuidiang <chuidiang@1f131f20-f6db-11dd-b556-9305e745af9e>
2009-03-25 19:19:49 +0000

    creado proyecto ejemplo-modelo-vista-presenter

---
+++ trunk/ejemplo-modelo-vista-presenter/src/main/java/com/chuidiang/App.java
@@ -0,0 +1,13 @@
+package com.chuidiang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
