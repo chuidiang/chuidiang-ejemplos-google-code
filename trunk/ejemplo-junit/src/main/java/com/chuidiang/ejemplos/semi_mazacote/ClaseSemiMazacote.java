6f5a59660e7d53ba506c0551633e6e802a17b5ac
chuidiang <chuidiang@1f131f20-f6db-11dd-b556-9305e745af9e>
2009-05-13 18:04:10 +0000

    Ejemplo de clase que no se puede probar y clase más fácilmente probable.

---
+++ trunk/ejemplo-junit/src/main/java/com/chuidiang/ejemplos/semi_mazacote/ClaseSemiMazacote.java
@@ -0,0 +1,47 @@
+package com.chuidiang.ejemplos.semi_mazacote;
+
+import java.util.Scanner;
+
+/**
+ * Main de ejemplo de una clase menos mazacote, más fácil de testear. En vez de
+ * leer directamnte de teclado y escribir de pantalla, lee de una interface y
+ * escribe en una interface
+ * 
+ * @author Chuidiang
+ */
+public class ClaseSemiMazacote {
+
+    /**
+     * Instancia la clase Sumador, le facilita implementaciones de las
+     * interfaces, y la pone a trabajar.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // Implementacion de IfzScanner, que lee de teclado.
+        IfzScanner unScanner = new IfzScanner() {
+            private final Scanner scanner = new Scanner(System.in);
+
+            public String nextLine() {
+                return scanner.nextLine();
+            }
+
+        };
+
+        // Implementacion de IfzMuestraResultados, que escribe en pantalla
+        IfzMuestraResultados muestraResultados = new IfzMuestraResultados() {
+            public void println(String textoPeticion) {
+                System.out.println(textoPeticion);
+            }
+        };
+
+        // Se crea Sumador y se inicializa.
+        Sumador sumador = new Sumador();
+        sumador.setIfzScanner(unScanner);
+        sumador.setIfzMuestraResultados(muestraResultados);
+
+        // Se le pone a trabajar.
+        sumador.empiezaATrabajar();
+    }
+}
