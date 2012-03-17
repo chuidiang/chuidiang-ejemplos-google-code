61ebf97fbc65019dd41fbd1865c08545809a5900
chuidiang <chuidiang@1f131f20-f6db-11dd-b556-9305e745af9e>
2012-03-17 17:40:55 +0000

    Rama para ws-security

---
+++ branches/ejemplo_cxf/ws_security_sobre_tomcat/src/test/java/com/chuidiang/ejemplos/cxf/ClientPasswordHandler.java
@@ -0,0 +1,20 @@
+package com.chuidiang.ejemplos.cxf;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.ws.security.WSPasswordCallback;
+
+public class ClientPasswordHandler implements CallbackHandler {
+
+   public void handle(Callback[] callbacks) throws IOException,
+         UnsupportedCallbackException {
+
+      WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+      pc.setPassword("password");
+   }
+
+}
