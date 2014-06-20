2de0458072b66f07a86e6ee5aaa62af0b6e1fa42
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-06-20 19:48:02 +0000

    Main minimo

---
+++ pruebas-worldwind/src/com/chuidiang/ejemplos/worldwind/MainMinimo.java
@@ -0,0 +1,33 @@
+package com.chuidiang.ejemplos.worldwind;
+
+import java.awt.BorderLayout;
+
+import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwindx.examples.LayerPanel;
+
+import javax.swing.JFrame;
+
+public class MainMinimo extends JFrame {
+
+   public MainMinimo() {
+      WorldWindowGLCanvas wwd = new WorldWindowGLCanvas();
+      wwd.setPreferredSize(new java.awt.Dimension(1000, 800));
+      this.getContentPane().add(wwd, java.awt.BorderLayout.CENTER);
+      wwd.setModel(new BasicModel());
+      
+      LayerPanel panelCapas = new LayerPanel(wwd);
+      this.getContentPane().add(panelCapas, BorderLayout.WEST);
+   }
+
+   public static void main(String[] args) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            JFrame frame = new MainMinimo();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+         }
+      });
+   }
+}
