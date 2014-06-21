36a375770ec8792b5532e8d390d82988d9a0ab8a
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-06-21 07:43:05 +0000

    Dibujo de tres tipos de poligonos: normal, extruido y de superficie

---
+++ pruebas-worldwind/src/org/chuidiang/chuwiki/worldwind/CapaPoligonos.java
@@ -0,0 +1,100 @@
+package org.chuidiang.chuwiki.worldwind;
+
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.BasicShapeAttributes;
+import gov.nasa.worldwind.render.ExtrudedPolygon;
+import gov.nasa.worldwind.render.Material;
+import gov.nasa.worldwind.render.Polygon;
+import gov.nasa.worldwind.render.ShapeAttributes;
+import gov.nasa.worldwind.render.SurfacePolygon;
+import gov.nasa.worldwindx.examples.ApplicationTemplate;
+import gov.nasa.worldwindx.examples.LayerPanel;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CapaPoligonos {
+   private WorldWindowGLCanvas canvas;
+   private LayerPanel panelCapas;
+   private RenderableLayer capa;
+
+   public CapaPoligonos(WorldWindowGLCanvas canvas, LayerPanel panelCapas) {
+      this.canvas = canvas;
+      this.panelCapas = panelCapas;
+      
+      capa = new RenderableLayer();
+      capa.setName("Poligonos");
+      ApplicationTemplate.insertBeforeCompass(canvas, capa);
+      panelCapas.update(canvas);
+      
+      // Forma alterntiva de añadir la capa.
+      // canvas.getModel().getLayers().add(capa);
+   }
+   
+   public void pintaPoligono(){
+      List<Position> vertices = new LinkedList<>();
+      vertices.add(Position.fromDegrees(40.0, -3.0, 700.0));
+      vertices.add(Position.fromDegrees(40.1, -3.0, 700.0));
+      vertices.add(Position.fromDegrees(40.1, -3.1, 700.0));
+      vertices.add(Position.fromDegrees(40.0, -3.1, 700.0));
+      
+      Polygon poligono = new Polygon(vertices);
+      // Los vertices podrian asignarse tambien con poligono.setOuterBoundary()
+      // en vez de en el constructor.
+      
+      ShapeAttributes atributos = new BasicShapeAttributes();
+      atributos.setOutlineMaterial(Material.RED);
+      atributos.setInteriorMaterial(Material.RED);
+      atributos.setInteriorOpacity(0.2);
+      poligono.setAttributes(atributos);
+      
+      capa.addRenderable(poligono);
+      canvas.redraw();
+   }
+   
+   public void pintaPoligonoExtruido(){
+      List<Position> vertices = new LinkedList<>();
+      vertices.add(Position.fromDegrees(40.2, -3.0, 600.0));
+      vertices.add(Position.fromDegrees(40.3, -3.0, 600.0));
+      vertices.add(Position.fromDegrees(40.3, -3.1, 600.0));
+      vertices.add(Position.fromDegrees(40.2, -3.1, 600.0));
+      
+      ExtrudedPolygon poligono = new ExtrudedPolygon(vertices,700.0);
+      // Los vertices podrian asignarse tambien con poligono.setOuterBoundary()
+      // en vez de en el constructor.
+      // La altura puede asignarse con poligono.setHeight()
+      
+      ShapeAttributes atributos = new BasicShapeAttributes();
+      atributos.setOutlineMaterial(Material.RED);
+      atributos.setDrawInterior(false);
+      poligono.setAttributes(atributos);
+      
+      capa.addRenderable(poligono);
+      canvas.redraw();
+   }
+   
+   public void pintaPoligonoSuperficie(){
+      List<Position> vertices = new LinkedList<>();
+      vertices.add(Position.fromDegrees(40.0, -3.2));
+      vertices.add(Position.fromDegrees(40.1, -3.2));
+      vertices.add(Position.fromDegrees(40.1, -3.3));
+      vertices.add(Position.fromDegrees(40.0, -3.3));
+      
+      SurfacePolygon poligono = new SurfacePolygon(vertices);
+      // Los vertices podrian asignarse tambien con poligono.setOuterBoundary()
+      // en vez de en el constructor.
+      
+      ShapeAttributes atributos = new BasicShapeAttributes();
+      atributos.setOutlineMaterial(Material.RED);
+      atributos.setInteriorMaterial(Material.RED);
+      atributos.setInteriorOpacity(0.2);
+      poligono.setAttributes(atributos);
+      
+      capa.addRenderable(poligono);
+      canvas.redraw();
+      
+   }
+
+}
--- pruebas-worldwind/src/org/chuidiang/chuwiki/worldwind/MainMinimo.java
+++ pruebas-worldwind/src/org/chuidiang/chuwiki/worldwind/MainMinimo.java
@@ -3,7 +3,13 @@ package org.chuidiang.chuwiki.worldwind;
 import java.awt.BorderLayout;
 
 import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.Configuration;
+import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.layers.ViewControlsLayer;
+import gov.nasa.worldwind.layers.ViewControlsSelectListener;
+import gov.nasa.worldwind.util.StatusBar;
+import gov.nasa.worldwindx.examples.ApplicationTemplate;
 import gov.nasa.worldwindx.examples.LayerPanel;
 
 import javax.swing.JFrame;
@@ -15,12 +21,35 @@ public class MainMinimo extends JFrame {
       wwd.setPreferredSize(new java.awt.Dimension(1000, 800));
       this.getContentPane().add(wwd, java.awt.BorderLayout.CENTER);
       wwd.setModel(new BasicModel());
-      
+
       LayerPanel panelCapas = new LayerPanel(wwd);
       this.getContentPane().add(panelCapas, BorderLayout.WEST);
+
+      StatusBar statusBar = new StatusBar();
+      this.getContentPane().add(statusBar, BorderLayout.PAGE_END);
+      statusBar.setEventSource(wwd);
+
+      ViewControlsLayer viewControlsLayer = new ViewControlsLayer();
+      ApplicationTemplate.insertBeforeCompass(wwd, viewControlsLayer);
+      wwd.addSelectListener(new ViewControlsSelectListener(wwd,
+            viewControlsLayer));
+
+      pintaPoligonos(wwd, panelCapas);
+   }
+
+   private void pintaPoligonos(WorldWindowGLCanvas wwd, LayerPanel panelCapas) {
+      CapaPoligonos capaPoligonos = new CapaPoligonos(wwd, panelCapas);
+      capaPoligonos.pintaPoligono();
+      capaPoligonos.pintaPoligonoExtruido();
+      capaPoligonos.pintaPoligonoSuperficie();
    }
 
    public static void main(String[] args) {
+
+      Configuration.setValue(AVKey.INITIAL_LATITUDE, Integer.valueOf(40));
+      Configuration.setValue(AVKey.INITIAL_LONGITUDE, Integer.valueOf(-3));
+      Configuration.setValue(AVKey.INITIAL_ALTITUDE, Integer.valueOf(190000));
+
       java.awt.EventQueue.invokeLater(new Runnable() {
          public void run() {
             JFrame frame = new MainMinimo();
