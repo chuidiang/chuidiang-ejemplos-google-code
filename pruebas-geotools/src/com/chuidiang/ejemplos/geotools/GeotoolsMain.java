5ea58c48422f66f3fa5011458a4b01a3ddde9d74
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-07-05 20:49:04 +0000

    Dibujar poligono desde codigo en GeoTools

--- pruebas-geotools/src/com/chuidiang/ejemplos/geotools/GeotoolsMain.java
+++ pruebas-geotools/src/com/chuidiang/ejemplos/geotools/GeotoolsMain.java
@@ -1,6 +1,7 @@
 package com.chuidiang.ejemplos.geotools;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 
@@ -9,63 +10,112 @@ import javax.swing.JToolBar;
 
 import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.collection.CollectionFeatureSource;
+import org.geotools.data.memory.MemoryFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.Layer;
 import org.geotools.map.MapContent;
 import org.geotools.styling.SLD;
 import org.geotools.styling.Style;
 import org.geotools.swing.JMapPane;
-import org.geotools.swing.SingleLayerMapContent;
 import org.geotools.swing.action.PanAction;
 import org.geotools.swing.action.ResetAction;
 import org.geotools.swing.action.ZoomInAction;
 import org.geotools.swing.action.ZoomOutAction;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Polygon;
 
 public class GeotoolsMain {
 
    public static void main(String[] args) throws IOException {
       // display a data store file chooser dialog for shapefiles
-      // Puedes descargar el fichero de http://www.naturalearthdata.com/downloads/10m-cultural-vectors/10m-admin-0-countries/
+      // Puedes descargar el fichero de
+      // http://www.naturalearthdata.com/downloads/10m-cultural-vectors/10m-admin-0-countries/
       File file = new File(
             "files/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp");
 
       FileDataStore store = FileDataStoreFinder.getDataStore(file);
       SimpleFeatureSource featureSource = store.getFeatureSource();
 
-      // Create a map content and add our shapefile to it
+      Style style = SLD.createSimpleStyle(featureSource.getSchema());
+      style=SLD.createPolygonStyle(Color.BLACK, Color.YELLOW, 0.2f);
+      
       MapContent map = new MapContent();
       map.setTitle("Quickstart");
-
-      Style style = SLD.createSimpleStyle(featureSource.getSchema());
 
       Layer layer = new FeatureLayer(featureSource, style);
       map.addLayer(layer);
       
+      CollectionFeatureSource propias = createFeatureType();
+      Layer layer2 = new FeatureLayer(propias, SLD.createPolygonStyle(Color.RED, Color.GREEN, 0.2f));
+      map.addLayer(layer2);
+
       // La siguiente linea dibuja un JFrame con el mapa y toda una
       // botoneria por defecto. Si llamamos a esta linea, el resto
       // de código sobra.
       // JMapFrame.showMap(map);
-      
-      
-      // Como no hemos llamado a la linea anterior, vamos a construir el 
+
+      // Como no hemos llamado a la linea anterior, vamos a construir el
       // JFrame y todos los botones.
-      JMapPane pane = new JMapPane(new SingleLayerMapContent(layer));
-      
+      JMapPane pane = new JMapPane(map);
+
       JToolBar toolBar = new JToolBar();
       toolBar.add(new ZoomInAction(pane));
       toolBar.add(new ZoomOutAction(pane));
       toolBar.add(new PanAction(pane));
       toolBar.add(new ResetAction(pane));
-      
+
       JFrame ventana = new JFrame("mapa");
       ventana.getContentPane().add(pane);
-      ventana.getContentPane().add(toolBar,BorderLayout.NORTH);
-      
+      ventana.getContentPane().add(toolBar, BorderLayout.NORTH);
+
       ventana.setSize(500, 500);
       ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       ventana.setVisible(true);
 
+   }
+
+   private static CollectionFeatureSource createFeatureType() {
+
+      SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+      builder.setName("Location");
+      builder.setSRS("EPSG:4326"); // <- Coordinate reference system
+
+      // add attributes in order
+      builder.add("the_geom", Polygon.class);
+      builder.length(15).add("Name", String.class); // <- 15 chars width for
+                                                    // name field
+      builder.add("number", Integer.class);
+
+      // build the type
+      final SimpleFeatureType LOCATION = builder.buildFeatureType();
+
+      SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(LOCATION);
+      GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
+
+      Coordinate[] coordinates = new Coordinate[] { new Coordinate(1.0, 1.0),
+            new Coordinate(1.0, 2.0), new Coordinate(2.0, 2.0),
+            new Coordinate(2.0, 1.0), new Coordinate(1.0, 1.0) };
+
+      Polygon poligono = geometryFactory.createPolygon(coordinates);
+
+      featureBuilder.add(poligono);
+      featureBuilder.add("el nombre");
+      featureBuilder.add(22);
+
+      MemoryFeatureCollection listFeatures = new MemoryFeatureCollection(
+            LOCATION);
+      listFeatures.add(featureBuilder.buildFeature(null));
+      CollectionFeatureSource featureSource = new CollectionFeatureSource(
+            listFeatures);
+      return featureSource;
    }
 
 }
