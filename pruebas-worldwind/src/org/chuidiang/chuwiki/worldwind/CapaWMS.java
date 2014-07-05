085442bafdab12f851c695dfd9a3fabba3a4faec
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-07-05 10:21:22 +0000

    formateado algo el codigo

--- pruebas-worldwind/src/org/chuidiang/chuwiki/worldwind/CapaWMS.java
+++ pruebas-worldwind/src/org/chuidiang/chuwiki/worldwind/CapaWMS.java
@@ -20,64 +20,68 @@ import gov.nasa.worldwindx.examples.LayerPanel;
 
 public class CapaWMS {
    private WMSTiledImageLayer capa;
-   
-   public CapaWMS(){
-      
-   }
-   
-   public void anadeUrl(String url, WorldWindowGLCanvas wwd, LayerPanel panelCapas ){
-      try {
-      WMSCapabilities capabilities = WMSCapabilities.retrieve(new URI(url));
-      capabilities.parse();
-      List<WMSLayerCapabilities> listaCapacidadesCapa = capabilities.getNamedLayers();
-      for (WMSLayerCapabilities capacidadesCapa : listaCapacidadesCapa){
-         
-         AVListImpl params = new AVListImpl();
-         params.setValue(AVKey.LAYER_NAMES, capacidadesCapa.getName());
-         Set<WMSLayerStyle> estilos = capacidadesCapa.getStyles();
-//         if (null != estilos){
-//         if (style != null)
-//             linfo.params.setValue(AVKey.STYLE_NAMES, style.getName());
-//         }
-         
-         
-         // Some wms servers are slow, so increase the timeouts and limits used by world wind's retrievers.
-         params.setValue(AVKey.URL_CONNECT_TIMEOUT, 30000);
-         params.setValue(AVKey.URL_READ_TIMEOUT, 30000);
-         params.setValue(AVKey.RETRIEVAL_QUEUE_STALE_REQUEST_LIMIT, 60000);
-//         params.setValue("transparent", "true");
-         
 
-         String factoryKey = getFactoryKeyForCapabilities(capabilities);
-         Factory factory = (Factory) WorldWind.createConfigurationComponent(factoryKey);
-         Object laCapa = factory.createFromConfigSource(capabilities, params);
-         if (laCapa instanceof Layer){
-            ((Layer)laCapa).setEnabled(false);
-            ApplicationTemplate.insertBeforeCompass(wwd, (Layer)laCapa);
-            panelCapas.update(wwd);
+   public CapaWMS() {
+
+   }
+
+   public void anadeUrl(String url, WorldWindowGLCanvas wwd,
+         LayerPanel panelCapas) {
+      try {
+         WMSCapabilities capabilities = WMSCapabilities.retrieve(new URI(url));
+         capabilities.parse();
+         List<WMSLayerCapabilities> listaCapacidadesCapa = capabilities
+               .getNamedLayers();
+         for (WMSLayerCapabilities capacidadesCapa : listaCapacidadesCapa) {
+
+            AVListImpl params = new AVListImpl();
+            params.setValue(AVKey.LAYER_NAMES, capacidadesCapa.getName());
+            Set<WMSLayerStyle> estilos = capacidadesCapa.getStyles();
+
+            // Codigo si queremos aplicar un estilo concreto en vez de el estilo
+            // por defecto.
+            // if (null != estilos){
+            // if (style != null)
+            // linfo.params.setValue(AVKey.STYLE_NAMES, style.getName());
+            // }
+
+            // Some wms servers are slow, so increase the timeouts and limits
+            // used by world wind's retrievers.
+            params.setValue(AVKey.URL_CONNECT_TIMEOUT, 30000);
+            params.setValue(AVKey.URL_READ_TIMEOUT, 30000);
+            params.setValue(AVKey.RETRIEVAL_QUEUE_STALE_REQUEST_LIMIT, 60000);
+
+            String factoryKey = getFactoryKeyForCapabilities(capabilities);
+            Factory factory = (Factory) WorldWind
+                  .createConfigurationComponent(factoryKey);
+            Object laCapa = factory
+                  .createFromConfigSource(capabilities, params);
+            
+            if (laCapa instanceof Layer) {
+               ((Layer) laCapa).setEnabled(false);
+               ApplicationTemplate.insertBeforeCompass(wwd, (Layer) laCapa);
+               panelCapas.update(wwd);
+            }
          }
-      }
-      } catch (Exception e){
+      } catch (Exception e) {
          e.printStackTrace();
       }
       wwd.redraw();
    }
-   
-  protected  String getFactoryKeyForCapabilities(WMSCapabilities caps)
-   {
-       boolean hasApplicationBilFormat = false;
 
-       Set<String> formats = caps.getImageFormats();
-       for (String s : formats)
-       {
-           if (s.contains("application/bil"))
-           {
-               hasApplicationBilFormat = true;
-               break;
-           }
-       }
+   protected String getFactoryKeyForCapabilities(WMSCapabilities caps) {
+      boolean hasApplicationBilFormat = false;
 
-       return hasApplicationBilFormat ? AVKey.ELEVATION_MODEL_FACTORY : AVKey.LAYER_FACTORY;
+      Set<String> formats = caps.getImageFormats();
+      for (String s : formats) {
+         if (s.contains("application/bil")) {
+            hasApplicationBilFormat = true;
+            break;
+         }
+      }
+
+      return hasApplicationBilFormat ? AVKey.ELEVATION_MODEL_FACTORY
+            : AVKey.LAYER_FACTORY;
    }
 
 }
