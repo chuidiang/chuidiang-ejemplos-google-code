5e9ccbeb152b80717c996202f4486b902cadba8f
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-07-09 18:45:20 +0000

    Faltaba el PanelCapas

---
+++ trunk/pruebas-geotools/src/com/chuidiang/ejemplos/geotools/PanelCapas.java
@@ -0,0 +1,40 @@
+package com.chuidiang.ejemplos.geotools;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+
+import org.geotools.map.Layer;
+
+public class PanelCapas extends JPanel{
+
+	public PanelCapas(Layer[] layers) {
+		super(new FlowLayout());
+		
+		for (Layer layer : layers) {
+			JCheckBox checkBox = new JCheckBox(layer.getTitle());
+			checkBox.addActionListener(new LayerListener(layer));
+			add(checkBox);
+		}
+	}
+
+	
+	class LayerListener implements ActionListener {
+		private Layer layer;
+		public LayerListener(Layer layer) {
+			this.layer=layer;
+		}
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (((JCheckBox)event.getSource()).isSelected()){
+				layer.setVisible(true);
+			} else {
+				layer.setVisible(false);
+			}
+			
+		}
+	}
+}
