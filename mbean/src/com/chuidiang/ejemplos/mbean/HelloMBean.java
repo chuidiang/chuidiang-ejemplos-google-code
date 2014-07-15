dbfd1ce58f05aef11a7477acd459a06ecbd98cd3
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-07-15 18:14:25 +0000

    Primer ejemplo tonto de mbean

---
+++ mbean/src/com/chuidiang/ejemplos/mbean/HelloMBean.java
@@ -0,0 +1,11 @@
+package com.chuidiang.ejemplos.mbean;
+
+/**
+ * MBean interface example. It must end with "MBean"
+ * @author chuidiang
+ *
+ */
+public interface HelloMBean {
+	int getCounter();
+	void reset();
+}
