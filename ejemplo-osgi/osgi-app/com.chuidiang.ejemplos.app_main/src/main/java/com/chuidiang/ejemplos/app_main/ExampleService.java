decd5eac5e0e1f512780731212698e47a87cec30
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-10-23 18:58:17 +0000

    Creados app principal, plugin1 en version 1 y 2 y plugin2. Ficheros de eclipse generados con mvn pax:eclpse

---
+++ ejemplo-osgi/osgi-app/com.chuidiang.ejemplos.app_main/src/main/java/com/chuidiang/ejemplos/app_main/ExampleService.java
@@ -0,0 +1,12 @@
+package com.chuidiang.ejemplos.app_main;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
