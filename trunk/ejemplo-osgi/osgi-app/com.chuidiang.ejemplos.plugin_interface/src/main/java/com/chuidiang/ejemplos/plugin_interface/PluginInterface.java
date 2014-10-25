6a2a61b26df96ca37a1197d0a69d282e4552d834
chuidiang@gmail.com <chuidiang@gmail.com@1f131f20-f6db-11dd-b556-9305e745af9e>
2014-10-25 11:56:30 +0000

    Añadido un poco de Swing

--- trunk/ejemplo-osgi/osgi-app/com.chuidiang.ejemplos.plugin_interface/src/main/java/com/chuidiang/ejemplos/plugin_interface/PluginInterface.java
+++ trunk/ejemplo-osgi/osgi-app/com.chuidiang.ejemplos.plugin_interface/src/main/java/com/chuidiang/ejemplos/plugin_interface/PluginInterface.java
@@ -1,10 +1,14 @@
 package com.chuidiang.ejemplos.plugin_interface;
 
+import java.awt.Component;
+
 /**
  * Public API representing an example OSGi service
  */
 public interface PluginInterface
 {
    String getName();
+
+   Component getComponent();
 }
 
