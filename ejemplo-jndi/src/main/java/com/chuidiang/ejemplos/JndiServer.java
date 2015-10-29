4095dd0908310a4e4da84fa03cccd3218b778288
chuidiang <chuidiang@gmail.com>
2015-10-29 22:14:33 +0100

    Cambiadas claves que son String por sus constantes correspondientes en la clase Context

--- ejemplo-jndi/src/main/java/com/chuidiang/ejemplos/JndiServer.java
+++ ejemplo-jndi/src/main/java/com/chuidiang/ejemplos/JndiServer.java
@@ -22,7 +22,7 @@ public class JndiServer {
 
    private static void storeSomeData() throws NamingException {
       Hashtable<String, String> env = new Hashtable<String, String>();
-      env.put("java.naming.factory.initial","org.jnp.interfaces.NamingContextFactory");
+      env.put(Context.INITIAL_CONTEXT_FACTORY,"org.jnp.interfaces.NamingContextFactory");
       env.put(Context.PROVIDER_URL,            "jnp://192.168.1.2:5400");
       Context context = new InitialContext(env);
       context.createSubcontext("config");
@@ -31,7 +31,7 @@ public class JndiServer {
    }
 
    private static void startServer() throws Exception, UnknownHostException {
-      System.setProperty("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
+      System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
 
       NamingBeanImpl jnpServer = new NamingBeanImpl();
       jnpServer.start();
